5cbc4f8181e113c886c898a8a7336093a7639d02
Ivo Fokkema <I.F.A.C.Fokkema@lumc.nl>
2015-10-08 19:48:57 +0000

    Diagnostics: Released 3.0-12e.
    git-svn-id: https://humgenprojects.lumc.nl/svn/LOVD3/branches/NGS_diagnostics@651 a7477d18-a37e-46de-aef1-57953c184492

--- src/inc-init.php
+++ src/inc-init.php
@@ -132,7 +132,7 @@ $aRequired =
 $_SETT = array(
                 'system' =>
                      array(
-                            'version' => '3.0-12d',
+                            'version' => '3.0-12e',
                           ),
                 'user_levels' =>
                      array(
