d62bdc1156845d0b2408fc5c621ad06e3df46072
Anthony Marty <martology@gmail.com>
2016-02-26 21:12:02 +1100

    Gene statistics search box
    - Created a search box to paste in a long list of gene names
    - Defaulted the search box to hide
    - Added a toggle button to the page to show and hide the search box
    - Handled showing the search box after posting data

--- src/gene_statistics.php
+++ src/gene_statistics.php
@@ -56,8 +56,15 @@ if (PATH_COUNT == 1 && !ACTION) {
     $_T->printHeader();
     $_T->printTitle();
 
+    $sGeneNames = '';
+    if (isset($_POST['geneNames'])) {
+        $sGeneNames = $_POST['geneNames'];
+    }
+
+    // TODO Validate the genes and alert if any of the genes are not within LOVD. Allow to proceed with only the found genes or allow the users to try and locate the correct genes
+    // TODO If we continue then write the genes to the session variable $_SESSION['viewlists'][$sViewListID]['checked'] and refresh the viewlist so as those genes are now checked
     // TODO BUG 1. Select genes 2. Only show selected genes 3. Sorting is disabled even though there are only a few genes selected 4. Only show selected genes again 5. Sorting is now enabled 6. Show all genes 7. Sorting is still enabled even though too many results are returned
-    // Objects.php, line 1052, I suspect since the setting and removing of the check filter does not change the search criteria it does not bother re counting the rows and as such uses the last record counts to determine if the sort should be enabled. Not sure then why it works if you activate the check filter twice...
+    // TODO BUG Continued Objects.php, line 1052, I suspect since the setting and removing of the check filter does not change the search criteria it does not bother re counting the rows and as such uses the last record counts to determine if the sort should be enabled. Not sure then why it works if you activate the check filter twice...
     ?>
     <script type="text/javascript">
         // This function toggles the checked filter
@@ -77,48 +84,49 @@ if (PATH_COUNT == 1 && !ACTION) {
             // Refresh the viewlist so as it can apply the checked filter
             setTimeout('lovd_AJAX_viewListSubmit(\'<?php print $sViewListID;?>\')', 0);
         }
+
+        $(document).ready(function() {
+        <?php if (!empty($sGeneNames)) { ?>
+            $('#genesForm').show();
+            $('#geneFormShowHide').val('show');
+            $('#searchBoxTitle').html('<b>Search for genes:</b>');
+        <?php } else { ?>
+            $('#genesForm').hide();
+            $('#geneFormShowHide').val('hide');
+            $('#searchBoxTitle').html('Show gene search box');
+        <?php } ?>
+
+            $("#searchBoxTitle").click(function(){
+                $("#genesForm").toggle('fast');
+                if ($('#geneFormShowHide').val() == 'show') {
+                    $('#geneFormShowHide').val('hide');
+                    $('#searchBoxTitle').html('Show gene search box');
+                } else {
+                    $('#geneFormShowHide').val('show');
+                    $('#searchBoxTitle').html('Search for genes:');
+                }
+            });
+        });
     </script>
 <?php
-    require ROOT_PATH . 'class/object_gene_statistics.php';
-    $_DATA = new LOVD_GeneStatistic();
-    // Redirect the link when clicking on genes to the genes info page
-    $_DATA->setRowLink($sViewListID, ROOT_PATH . 'genes/' . $_DATA->sRowID);
-    // Allow users to download this gene statistics selected gene list
-    print('      <UL id="viewlistMenu_' . $sViewListID . '" class="jeegoocontext jeegooviewlist">' . "\n");
-    print('        <LI class="icon"><A click="lovd_AJAX_viewListSubmit(\'' . $sViewListID . '\', function(){lovd_AJAX_viewListCheckedFilter(true);});"><SPAN class="icon" style="background-image: url(gfx/check.png);"></SPAN>Show only checked genes</A></LI>' . "\n");
-    print('        <LI class="icon"><A click="lovd_AJAX_viewListSubmit(\'' . $sViewListID . '\', function(){lovd_AJAX_viewListCheckedFilter(false);});"><SPAN class="icon" style="background-image: url(gfx/cross_disabled.png);"></SPAN>Show all genes</A></LI>' . "\n");
-    print('        <LI class="icon"><A click="lovd_AJAX_viewListSubmit(\'' . $sViewListID . '\', function(){lovd_AJAX_viewListDownload(\'' . $sViewListID . '\', false);});"><SPAN class="icon" style="background-image: url(gfx/menu_save.png);"></SPAN>Download selected genes</A></LI>' . "\n");
-    print('      </UL>' . "\n\n");
-    $_DATA->viewList($sViewListID, array(), false, false, (bool) ($_AUTH['level'] >= LEVEL_SUBMITTER));
 
-    $_T->printFooter();
-    exit;
-}
+print('<div id="searchBoxTitle" style="cursor: pointer;text-decoration: underline;font-size : 11px;font-weight: bold"></div>');
+print('<form id="genesForm" method="post" style="display: none;">Enter in you list of gene symbols separated by commas and press search.<BR><input type="hidden" id="geneFormShowHide" value="hide"><textarea rows="5" cols="200" name="geneNames" id="geneNames">' . $sGeneNames . '</textarea><BR><input type="submit" name="submitGenes" id="submitGenes" value=" Search "></form>');
 
+require ROOT_PATH . 'class/object_gene_statistics.php';
+$_DATA = new LOVD_GeneStatistic();
+// Redirect the link when clicking on genes to the genes info page
+$_DATA->setRowLink($sViewListID, ROOT_PATH . 'genes/' . $_DATA->sRowID);
+// Allow users to download this gene statistics selected gene list
+print('      <UL id="viewlistMenu_' . $sViewListID . '" class="jeegoocontext jeegooviewlist">' . "\n");
+print('        <LI class="icon"><A click="lovd_AJAX_viewListSubmit(\'' . $sViewListID . '\', function(){lovd_AJAX_viewListCheckedFilter(true);});"><SPAN class="icon" style="background-image: url(gfx/check.png);"></SPAN>Show only checked genes</A></LI>' . "\n");
+print('        <LI class="icon"><A click="lovd_AJAX_viewListSubmit(\'' . $sViewListID . '\', function(){lovd_AJAX_viewListCheckedFilter(false);});"><SPAN class="icon" style="background-image: url(gfx/cross_disabled.png);"></SPAN>Show all genes</A></LI>' . "\n");
+print('        <LI class="icon"><A click="lovd_AJAX_viewListSubmit(\'' . $sViewListID . '\', function(){lovd_AJAX_viewListDownload(\'' . $sViewListID . '\', false);});"><SPAN class="icon" style="background-image: url(gfx/menu_save.png);"></SPAN>Download selected genes</A></LI>' . "\n");
+print('      </UL>' . "\n\n");
+$_DATA->viewList($sViewListID, array(), false, false, (bool) ($_AUTH['level'] >= LEVEL_SUBMITTER));
 
-
-
-
-if (PATH_COUNT == 1 && ACTION == 'search') {
-// URL: /gene_statistics?search
-// Search on a long list of genes that can be pasted into the page.
-    
-    // TODO Create a popup lovd_openWindow() that will allow the users to paste in the long list of genes
-    // TODO Validate the genes and alert if any of the genes are not within LOVD. Allow to proceed with only the found genes or allow the users to try and locate the correct genes
-    // TODO If we continue then write the genes to the session variable $_SESSION['viewlists'][$sViewListID]['checked'] and refresh the viewlist so as those genes are now checked
-
-    lovd_requireAUTH(LEVEL_SUBMITTER);
-    define('PAGE_TITLE', 'Gene statistics - Batch gene upload');
-    $_T->printHeader();
-    $_T->printTitle();
-
-    // This is an example of how you can set the genes to be already checked in the gene statistics viewlist. This will overwrite the existing checked genes so handle with care.
-    // $_SESSION['viewlists'][$sViewListID]['checked'] = array('A1BG','A1CF','A2M-AS1','A2MP1','A4GNT');
-
-    print('Paste in comma separated values to be used for selecting a large number of genes');
-
-    $_T->printFooter();
-    exit;
+$_T->printFooter();
+exit;
 }
 
 
@@ -129,7 +137,7 @@ if (PATH_COUNT == 1 && ACTION == 'search') {
 define('PAGE_TITLE', 'View gene statistics');
 $_T->printHeader();
 $_T->printTitle();
-print ('Incorrect use of the gene statistics page, please <a href="' . ROOT_PATH . $_PE[0] . '">click here</a> to view all the gene statistics.<br><br><br><br>');
+print ('Incorrect use of the gene statistics page, please <a href="' . $_PE[0] . '">click here</a> to view all the gene statistics.<br><br><br><br>');
 $_T->printFooter();
 exit;
 
