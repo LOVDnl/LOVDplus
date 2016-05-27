154828784b58c96e651512361325bacad859b17f
jplazzer <johnpaul.plazzer@gmail.com>
2016-05-27 11:36:56 +1000

    Summary Annotations initial code: - Created summary_annotations.php file and new rewriteRule. - Added code to create a new summary annotation entry and then switch to the edit form page. - Changed references to "general_annotations" to "summary_annotations" in variants.php as this is where the summary_annotations.php is accessed from.

--- src/summary_annotations.php
+++ src/summary_annotations.php
@@ -38,3 +38,64 @@ if ($_AUTH) {
     require ROOT_PATH . 'inc-upgrade.php';
 }
 
+
+
+if (PATH_COUNT == 2 && ACTION == 'create') {
+//if ( ACTION == 'create') {
+    // URL: /summary_annotations/chrX_000030?create
+    // Create a new summary annotation entry.
+
+    $DBID = sprintf('%s', $_PE[1]);
+    $nVariantID = $_GET['variant_id'];
+
+    define('PAGE_TITLE', 'Create a new summary annotation entry');
+    define('LOG_EVENT', 'SummaryAnnotationCreate');
+
+    lovd_requireAUTH(LEVEL_ANALYZER);
+
+
+    require ROOT_PATH . 'class/object_general_annotations.php';
+    $_DATA = new LOVD_GeneralAnnotation();
+    require ROOT_PATH . 'inc-lib-form.php';
+
+    //if (!empty($_POST)) {
+    if (empty($_POST)) {
+            lovd_errorClean();
+            //$_DATA->checkFields($_POST);
+
+            if (!lovd_error()) {
+                // Fields to be used.
+                $aFields = array( 'id', 'effectid', 'created_by', 'created_date');
+
+                // Prepare values.
+                $_POST['id'] = $DBID;
+                $_POST['effectid'] = 11;
+                $_POST['created_by'] = $_AUTH['id'];
+                $_POST['created_date'] = date('Y-m-d H:i:s');
+
+                $nID = $_DATA->insertEntry($_POST, $aFields);
+
+                // Write to log...
+                lovd_writeLog('Event', LOG_EVENT, 'Created summary annotation entry ' . $nID . ' - ' . $_POST['name']);
+
+                header('Refresh: 0; url=' . lovd_getInstallURL() . CURRENT_PATH . '?edit&variant_id=' . $nVariantID);
+
+            //    $_T->printHeader();
+            //    $_T->printTitle();
+           //     lovd_showInfoTable('Successfully created the summary annotation entry!', 'success');
+
+            //    $_T->printFooter();
+                exit;
+            }
+
+        } else {
+            $_DATA->setDefaultValues();
+        }
+
+}
+
+
+
+
+print('No condition met using the provided URL.');
+?>
--- src/variants.php
+++ src/variants.php
@@ -512,7 +512,7 @@ if (PATH_COUNT == 2 && ctype_digit($_PE[1]) && !ACTION) {
         $zData = $_DATA->viewEntry($sGeneralAnnotationsID);
 
         $aNavigation = array();
-        $aNavigation['general_annotations/' . $sGeneralAnnotationsID . '?edit&variant_id=' . $nID]       = array('menu_edit.png', 'Edit general annotations entry', 1);
+        $aNavigation['summary_annotations/' . $sGeneralAnnotationsID . '?edit&variant_id=' . $nID]       = array('menu_edit.png', 'Edit general annotations entry', 1);
         lovd_showJGNavigation($aNavigation, 'GeneralAnnotations');
 
     } else {
@@ -521,7 +521,7 @@ if (PATH_COUNT == 2 && ctype_digit($_PE[1]) && !ACTION) {
               '          <TR>' . "\n" .
               '            <TH colspan="2">General annotations</TH>' . "\n" .
               '          </TR>' . "\n" .
-              '          <TR class="pointer" onclick="window.location.href=\'' . lovd_getInstallURL() . 'general_annotations/' . $zData['dbid'] . '?create&variant_id=' . $nID . '\';">' . "\n" .
+              '          <TR class="pointer" onclick="window.location.href=\'' . lovd_getInstallURL() . 'summary_annotations/' . $zData['dbid'] . '?create&variant_id=' . $nID . '\';">' . "\n" .
               '            <TD align="center" width="40"><IMG src="gfx/lovd_variants_create.png" alt="General annotations" width="32" height="32"></TD>' . "\n" .
               '            <TD>Annotations that may be applicable to any instance of a particular variant can be stored in a general annotations entry. Click here to create a general annotation entry for this variant.</TD>' . "\n" .
               '          </TR>' . "\n" .
