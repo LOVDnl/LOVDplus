df8402a1717d5a440b453d3c10105c8515f79914
Anthony Marty <martology@gmail.com>
2016-06-29 15:57:47 +1000

    Fix missing link for adding gene statistics when the table is empty.

--- src/class/objects.php
+++ src/class/objects.php
@@ -1007,7 +1007,11 @@ class LOVD_Object {
             if ($bOnlyRows) {
                 die('0'); // Silent error.
             }
-            lovd_showInfoTable('No entries in the database yet!', 'stop');
+            if ($sViewListID == 'GeneStatistic') {
+                lovd_showInfoTable('No gene statistics have been imported into this database. Please <A href="gene_statistics?import">click here</A> to import them.', 'stop');
+            } else {
+                lovd_showInfoTable('No entries in the database yet!', 'stop');
+            }
             return 0;
         }
 
