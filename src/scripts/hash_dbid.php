455d8027ff1a1c1ad888c49b0a7839bee227f8a6
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2017-11-13 19:14:33 +0100

    Also included code to have Leiden databases lose their old LOVD3-style DBIDs. - An old import method led to gene-specific DBIDs, that need to be cleaned up as well. - The optimized updating method doesn't match these, so they need to be adapted to match first. - The delay for databases to check whether these old DBIDs are present, is minimized as much as possible.

--- src/scripts/hash_dbid.php
+++ src/scripts/hash_dbid.php
@@ -4,7 +4,7 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2017-06-05
- * Modified    : 2017-11-03
+ * Modified    : 2017-11-13
  * For LOVD+   : 3.0-18
  *
  * Copyright   : 2004-2017 Leiden University Medical Center; http://www.LUMC.nl/
@@ -29,6 +29,9 @@ $_SERVER = array_merge($_SERVER, array(
 ));
 require ROOT_PATH . 'inc-init.php';
 require ROOT_PATH . 'inc-lib-form.php';
+
+// Really don't buffer anything.
+@ob_end_flush();
 
 ////////////////////////////////
 ///// Script starts here //////
@@ -100,6 +103,31 @@ print(date('Y-m-d H:i:s') . ": Finished updating summary annotation log entries!
 
 
 // Final step: Update VariantOnGenome/DBID in TABLE_VARIANTS.
+// First, check if there are any "old" DBIDs. "Old" DBIDs are in the LOVD3
+//  format, and gene-specific. They are not picked up by the big update query
+//  all the way below, because that would be too slow. If there are any, update
+//  those first to "chr", after which the rest of the script will fix them
+//  properly.
+// Not sure how efficient that is, but this should really only affect the Leiden
+//  instances that used to have a different import method that used to generate
+//  gene-specific DBIDs.
+// A SELECT can use the index on the column, an update doesn't.
+// So do a quick SELECT first, and make this as fast as possible.
+// Such a "SELECT 1 ... LIMIT 1" is just as fast as a COUNT(*) when no matches
+//  are found, but much much faster when matches *are* found.
+$bOldDBIDs = (bool) $_DB->query('SELECT 1 FROM ' . TABLE_VARIANTS . ' WHERE BINARY `VariantOnGenome/DBID` REGEXP "^[A-Z]" LIMIT 1')->fetchColumn();
+if ($bOldDBIDs) {
+    // At least one old DBID found; run an update, we hope this isn't that many variants.
+    print(date('Y-m-d H:i:s') . ": Old LOVD3-style DBIDs found, running one large update query to get rid of those first...\n");
+    // Running an update twice over the data is indeed slower, but I don't want to
+    //  duplicate the below code and we'll only run this once on Leiden instances only.
+    // The "chr?" will make clear it's a invalid value, but will trigger the below code.
+    $nUpdated = $_DB->query('UPDATE ' . TABLE_VARIANTS . ' SET `VariantOnGenome/DBID` = "chr?" WHERE BINARY `VariantOnGenome/DBID` REGEXP "^[A-Z]"')->rowCount();
+    print(date('Y-m-d H:i:s') . ": Cleaned up $nUpdated old LOVD3-style DBIDs.\n");
+}
+
+
+
 // The LIKE "ch%" makes sure we don't match any SHA1 results, as that cannot include an 'h'.
 $sSQL = 'SELECT COUNT(id) AS num FROM ' . TABLE_VARIANTS . ' WHERE `VariantOnGenome/DBID` LIKE "ch%"';
 $nRow = $_DB->query($sSQL, array())->fetchColumn();
@@ -111,15 +139,16 @@ if (!$nRow) {
 $nBatchSize = 10000;
 $nBatches = ceil($nRow/$nBatchSize);
 
-print('Rows: ' . $nRow . "\n");
-print('Batches: ' . $nBatches . "\n");
+print('Rows: ' . $nRow . "\n" .
+      'Batches: ' . $nBatches . "\n" .
+      date('Y-m-d H:i:s') . ': Running updates to implement the new DBID...' . "\n");
+flush();
 
 // TODO: WARNING! UPDATE THIS QUERY WHENEVER lovd_fetchDBID() IS UPDATED!
 $sSQLUpdateDBID = 'UPDATE '  . TABLE_VARIANTS . ' SET `VariantOnGenome/DBID` = SHA1(CONCAT("' . $_CONF['refseq_build'] . '.chr", chromosome, ":", REPLACE(REPLACE(REPLACE(`VariantOnGenome/DNA`, "(", ""), ")", ""), "?", ""))) 
                    WHERE `VariantOnGenome/DBID` LIKE "ch%"
                    LIMIT ' . $nBatchSize;
 $zUpdateDBIDQuery = $_DB->prepare($sSQLUpdateDBID);
-
 
 for ($nBatch = 1; $nBatch <= $nBatches; $nBatch++) {
     $zUpdateDBIDQuery->execute();
