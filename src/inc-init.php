16d741595136fc85a1a44d4e4c8db685b1ab4dee
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2018-12-20 17:27:46 +0100

    Removed references to "required" XML functions, that we don't use.

--- src/inc-init.php
+++ src/inc-init.php
@@ -132,14 +132,12 @@ $aRequired =
                 'PHP_functions' =>
                      array(
                             'mb_detect_encoding',
-                            'xml_parser_create', // We could also look for libxml constants?
                             'openssl_seal',      // We could also look for openssl constants?
                             'curl_init',
                             'mime_content_type', // For the attachment uploads.
                           ),
                 'PHP_classes' =>
                      array(
-                            'SoapClient',
                           ),
                 'MySQL' => '4.1.2',
               );
